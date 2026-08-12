241fee5734b4b5a32c906c786a1e2e807fd08e92
Archana <rathourarchna922@gmail.com>
2026-08-13 00:43:34 +0530

    Add comment model

---
+++ backend/models/Comment.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
